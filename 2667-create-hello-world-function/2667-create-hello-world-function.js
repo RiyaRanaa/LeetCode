787a745bc3016449200847857d399fdc962b8381
Riya Rana <114066181+RiyaRanaa@users.noreply.github.com>
2024-06-30 10:06:29 +0530

    Time: 60 ms (9.46%), Space: 48.4 MB (83.33%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+const createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
